d1f8b9e517e979fb4bf0dcba6eec078912183276
Matt Rubin <58916601+mrubin3@users.noreply.github.com>
2024-07-16 12:25:13 -0400

    Update livelookup.php

--- livelookup.php
+++ livelookup.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-type: text/xml');
-echo '<?xml version="1.0" encoding="utf-8"?>';
 ?>
+<?xml version="1.0" encoding="utf-8"?>
 <livelookup version="1.0" columns="customer_id, last_name">
   <customer>
-    <customer_id><?php echo 'New Test' ?></customer_id>
+    <customer_id><?php echo 'New Test'; ?></customer_id>
     <last_name>test2</last_name>
     <phone>555-222-3344</phone>
   </customer>
